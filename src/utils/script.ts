6ccdfd4648bd7427eb163501979aad3074c223a3
ahonn <ahonn95@outlook.com>
2023-08-29 23:31:17 +0800

    fix: fix getAnyoneCanPayMinimumCapacity

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -73,13 +73,19 @@ export function getAnyoneCanPayMinimumCapacity(script: Script) {
   if (!isAnyoneCanPay(script)) {
     return 0;
   }
+
+  // https://blog.cryptape.com/omnilock-a-universal-lock-that-powers-interoperability-1#heading-anyone-can-pay-mode
   if (isOmnilockScript(script)) {
     const minimumCKB = BI.from(`0x${script.args.slice(46, 48)}`).toNumber();
     return 10 ** minimumCKB;
   }
 
+  // https://github.com/nervosnetwork/rfcs/blob/master/rfcs/0026-anyone-can-pay/0026-anyone-can-pay.md#script-structure
   if (isAnyoneCanPayScript(script)) {
-    const minimumCKB = BI.from(`0x${script.args.slice(46, 48)}`).toNumber();
+    if (script.args.length === 42) {
+      return 0;
+    }
+    const minimumCKB = BI.from(`0x${script.args.slice(42, 44)}`).toNumber();
     return 10 ** minimumCKB;
   }
 
