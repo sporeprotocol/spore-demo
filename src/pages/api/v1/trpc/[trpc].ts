5c4a846c68f5aa579e9ad3c3c17367a6d413c43f
ahonn <ahonn95@outlook.com>
2023-10-31 11:54:45 +0800

    feat: set trpc api maxDuration to 300s

--- src/pages/api/v1/trpc/[trpc].ts
+++ src/pages/api/v1/trpc/[trpc].ts
@@ -2,6 +2,8 @@ import { createContext } from '@/server/context';
 import { appRouter } from '@/server/routers';
 import * as trpcNext from '@trpc/server/adapters/next';
 
+export const maxDuration = 300;
+
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
