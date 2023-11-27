8a08ed6dd94d718022d622193050ae0f341fbc3a
ahonn <ahonn95@outlook.com>
2023-11-27 21:33:37 +0800

    feat: improve GraphQL caching and config
    - Introduce advanced caching strategies to GraphQL API using Apollo plugins and custom key-value store
    - Extend GraphQL context for optional cache disabling
    - Configure Apollo server with enhanced cache management and introspection enabled
    - Update dependencies with new GraphQL utilities and libraries for improved performance
    - Secure project by ignoring environment-specific `.env` files in version control

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,10 +1,53 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { createContext, createApolloServer } from 'spore-graphql';
+import { createContext, createApolloServer, ContextValue } from 'spore-graphql';
+import responseCachePlugin from '@apollo/server-plugin-response-cache';
+import { ApolloServerPluginCacheControl } from '@apollo/server/plugin/cacheControl';
+import { KeyvAdapter } from '@apollo/utils.keyvadapter';
+import { kv } from '@vercel/kv';
+import Keyv, { Store } from 'keyv';
 
 export const config = {
   maxDuration: 300,
 };
 
-export default startServerAndCreateNextHandler(createApolloServer({}), {
-  context: async () => createContext(),
-});
+type CustomContext = ContextValue & {
+  disableCache?: boolean;
+};
+
+const store: Store<string> = {
+  async get(key: string): Promise<string | undefined> {
+    const val = await kv.get(key);
+    return val as string | undefined;
+  },
+  async set(key: string, value: string, ttl?: number | undefined) {
+    if (ttl) {
+      return kv.set(key, value, { px: ttl });
+    }
+    return kv.set(key, value);
+  },
+  async delete(key: string): Promise<boolean> {
+    const count = await kv.del(key);
+    return count === 1;
+  },
+  async clear(): Promise<void> {
+    await kv.flushall();
+  },
+};
+
+const cache = new KeyvAdapter(new Keyv({ store }));
+
+export default startServerAndCreateNextHandler(
+  createApolloServer<CustomContext>({
+    introspection: true,
+    cache,
+    plugins: [
+      ApolloServerPluginCacheControl({
+        defaultMaxAge: 60 * 24,
+      }),
+      responseCachePlugin(),
+    ],
+  }),
+  {
+    context: async () => createContext(),
+  },
+);
