056ba4b77fec9745a605a24c2c4e2b6bba6ee8cc
ShookLyngs <shooklyngs@gmail.com>
2024-01-12 17:08:08 +0800

    bump: spore-sdk to 0.1.1-beta.0

--- src/config.ts
+++ src/config.ts
@@ -1,40 +1,21 @@
-import { SporeConfig } from '@spore-sdk/core';
+import { predefinedSporeConfigs, SporeConfig } from '@spore-sdk/core';
 import { predefined } from "@ckb-lumos/config-manager";
 
 export const sporeConfig: SporeConfig = {
-  lumos: predefined.AGGRON4,
-  ckbNodeUrl: 'https://testnet.ckb.dev/rpc',
-  ckbIndexerUrl: 'https://testnet.ckb.dev/indexer',
-  maxTransactionSize: 500 * 1024,
-  scripts: {
-    Spore: {
-      script: {
-        codeHash: '0x86973384d661ac5aabd581de082b04726d74705fa0f97602faf10089be0a0f85',
-        hashType: 'data1',
+  ...predefinedSporeConfigs.Aggron4,
+  lumos: {
+    ...predefined.AGGRON4,
+    SCRIPTS: {
+      ...predefined.AGGRON4.SCRIPTS,
+      OMNILOCK: {
+        CODE_HASH:
+          "0xf329effd1c475a2978453c8600e1eaf0bc2087ee093c3ee64cc96ec6847752cb",
+        HASH_TYPE: "type",
+        TX_HASH:
+          "0xb50ef6f2e9138f4dbca7d5280e10d29c1a65e60e8a574c009a2fa4e4107e0750",
+        INDEX: "0x0",
+        DEP_TYPE: "code",
       },
-      cellDep: {
-        outPoint: {
-          txHash: '0x024b39f1e840548a5f4c915ced950a79dbd0c78954290e472c995eb83f72b7cf',
-          index: '0x0',
-        },
-        depType: 'code',
-      },
-      versions: [],
-    },
-    Cluster: {
-      script: {
-        codeHash: '0x3df619d2d3b80b561394c57df504f3f13e1a1e3a0ea6f3f61ad2cc1da5af9911',
-        hashType: 'data1',
-      },
-      cellDep: {
-        outPoint: {
-          txHash: '0x5382cd28be1da97b2e7f2824b68e3e5613441b489031eab7d1346fe4a0b9a0cf',
-          index: '0x0',
-        },
-        depType: 'code',
-      },
-      versions: [],
     },
   },
-  extensions: [],
 };
