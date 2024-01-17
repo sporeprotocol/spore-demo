54046f9ca21a182122127a869be85767446759e5
ShookLyngs <shooklyngs@gmail.com>
2024-01-17 22:30:46 +0800

    bump: spore-sdk to 0.2.0-beta.0, lumos to 0.22.0-next.3

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,6 @@
 import { predefinedSporeConfigs, setSporeConfig, SporeConfig } from '@spore-sdk/core';
-import { predefined } from "@ckb-lumos/config-manager";
 
-const sporeConfig: SporeConfig = {
-  ...predefinedSporeConfigs.Aggron4,
-  lumos: {
-    ...predefined.AGGRON4,
-    SCRIPTS: {
-      ...predefined.AGGRON4.SCRIPTS,
-      OMNILOCK: {
-        CODE_HASH:
-          "0xf329effd1c475a2978453c8600e1eaf0bc2087ee093c3ee64cc96ec6847752cb",
-        HASH_TYPE: "type",
-        TX_HASH:
-          "0xb50ef6f2e9138f4dbca7d5280e10d29c1a65e60e8a574c009a2fa4e4107e0750",
-        INDEX: "0x0",
-        DEP_TYPE: "code",
-      },
-    },
-  },
-};
+const sporeConfig: SporeConfig = predefinedSporeConfigs.Aggron4;
 
 setSporeConfig(sporeConfig);
 
