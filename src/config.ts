59f18da4e619904bdddc5dda7236ccad0563d458
ShookLyngs <shooklyngs@gmail.com>
2024-01-21 22:06:05 +0800

    bump: upgrade omnilock's dep info in lumos config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,18 @@
-import { predefinedSporeConfigs, setSporeConfig, SporeConfig } from '@spore-sdk/core';
+import { forkSporeConfig, predefinedSporeConfigs, setSporeConfig, SporeConfig } from '@spore-sdk/core';
 
-const sporeConfig: SporeConfig = predefinedSporeConfigs.Aggron4;
+const sporeConfig: SporeConfig = forkSporeConfig(predefinedSporeConfigs.Aggron4, {
+  lumos: {
+    PREFIX: 'ckt',
+    SCRIPTS: {
+      ...predefinedSporeConfigs.Aggron4.lumos.SCRIPTS,
+      OMNILOCK: {
+        ...predefinedSporeConfigs.Aggron4.lumos.SCRIPTS.OMNILOCK!,
+        TX_HASH: '0x3d4296df1bd2cc2bd3f483f61ab7ebeac462a2f336f2b944168fe6ba5d81c014',
+        INDEX: '0x0',
+      },
+    },
+  },
+});
 
 setSporeConfig(sporeConfig);
 
